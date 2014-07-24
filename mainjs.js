6fac6aafda33b6d6e224a98ee38dc64e1093f13b
amitabha ghosh <amitabhaghosh197@gmail.com>
2014-07-24 16:14:11 +0530

    // JavaScript Document jQuery(function($){
    	 "use strict";
    
    	  $('#slider').flexslider({
            animation: "slide",
            prevText: '<img src="imgs/pre.png">',
            nextText: '<img src="imgs/next.png">',
    
        });
    //Clear out clones in jquery.flexslider.js in the line 874
    
    	});//function ends

---
+++ mainjs.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+jQuery(function($){
+	
+	 "use strict";
+	 
+	  $('#slider').flexslider({
+        animation: "slide",
+        prevText: '<img src="imgs/pre.png">',
+        nextText: '<img src="imgs/next.png">',
+		 
+	
+    });
+//Clear out clones in jquery.flexslider.js in the line 874
+	
+	
+	
+	});//function ends
